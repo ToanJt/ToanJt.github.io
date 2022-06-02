0d4077f574d0129f688b0b18079402e2e13d05bb
Toan <lexuantoan5703@gmail.com>
2022-06-02 22:35:45 +0700

    j

--- ALABATA/script.js
+++
@@ -1,7 +0,0 @@
-var index=1;
-changeImage = function() {
-    var imgs=["/image/banner1.jpeg","/image/banner2.jpeg","/image/banner3.jpg"];
-    document.getElementById('img').scr = imgs[index];
-    index++;
-}
-setInterval(changeImage, 3000);
